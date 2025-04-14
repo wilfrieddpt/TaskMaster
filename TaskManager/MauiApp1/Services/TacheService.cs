ac7fb1397252cdbc048ba7db24c39f65c4ade28f
engonzalez1 <enzo.gonzalez2@etu.uca.fr>
2025-04-14 14:28:17 +0200

    Ajout du dossier et fichier de Services/TacheService.cs

---
+++ TaskManager/MauiApp1/Services/TacheService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MauiApp1.Models;
+using MauiApp1.Data;
+
+namespace MauiApp1.Services
+{
+    public class TacheService
+    {
+        private readonly AppDbContext _context;
+
+        public TacheService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Tache>> GetTachesAsync()
+        {
+            return await _context.Taches.ToListAsync();
+        }
+    }
+}
